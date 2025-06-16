a72e1f8dc46e69468e3112e27a7d11a3e8c5c32a
Patrycja Szpakowska <patrycja.szpakowska@gmail.com>
2025-06-16 12:16:41 +0200

    Add swagger

--- RetakeTest2/Program.cs
+++ RetakeTest2/Program.cs
@@ -1,3 +1,6 @@
+using Microsoft.OpenApi.Models;
+using Swashbuckle.AspNetCore.SwaggerUI;
+
 namespace RetakeTest2;
 
 public class Program
@@ -11,8 +14,40 @@ public class Program
 
         // Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
         builder.Services.AddOpenApi();
+        builder.Services.AddSwaggerGen(c =>
+        {
+            c.SwaggerDoc("v1", new OpenApiInfo
+            {
+                Title = "Event API",
+                Version = "v1",
+                Description = "API for Managing Backpacks",
+                Contact = new OpenApiContact
+                {
+                    Name = "API Support",
+                    Email = "support@example.com",
+                    Url = new Uri("https://example.com/support")
+                },
+                License = new OpenApiLicense
+                {
+                    Name = "MIT License",
+                    Url = new Uri("https://opensource.org/licenses/MIT")
+                }
+            });
+        });
 
         var app = builder.Build();
+        if (app.Environment.IsDevelopment())
+        {
+            app.UseSwagger();
+            app.UseSwaggerUI(c =>
+            {
+                c.SwaggerEndpoint("/swagger/v1/swagger.json", "Backpack API v1");
+                c.DocExpansion(DocExpansion.List);
+                c.DefaultModelsExpandDepth(0);
+                c.DisplayRequestDuration();
+                c.EnableFilter();
+            });
+        }
 
         // Configure the HTTP request pipeline.
         if (app.Environment.IsDevelopment())
